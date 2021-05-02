d03774e6546d72b72182d4c42fd3451c5d525862
hscottvo <hscottvo@gmail.com>
2021-05-02 14:18:09 -0700

    Update main.c

--- source/main.c
+++ source/main.c
@@ -12,12 +12,24 @@
 #include "simAVRHeader.h"
 #endif
 
+void ADC_init() {
+    ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+}
+
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
     /* Insert your solution below */
-    while (1) {
+    unsigned short val = 0;
 
+    ADC_init();
+
+    while (1) {
+        val = ADC; 
+        PORTB = (char)val;
+        PORTD = (char)((val >> 8) & 0x03);
     }
     return 1;
 }
